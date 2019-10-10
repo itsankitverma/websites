fabcedc7cd31d9ea69baca00a9ce73d49e87fa6e
itsankitverma <45737260+itsankitverma@users.noreply.github.com>
2019-10-10 19:40:00 +0530

    websites made for teesting

---
+++ newWebsite/script.js
@@ -0,0 +1,16 @@
+
+
+
+   
+
+    // alert("Please enter the amount to be calculated among yourselves");
+    // var a = prompt(); 
+    // document.write("The amount entered here is : "+a);
+    // alert("Please enter the number of people to calcuted the split amoung");
+    // var b = prompt(); 
+    // document.write("");
+    // document.write("<br><br>The number of people are : "+b);
+    // document.write("<br><br>");
+    // alert("Please pay : "+(a/b)+" this amount")
+    // document.write("The split amount is : "+(a/b));
+    
